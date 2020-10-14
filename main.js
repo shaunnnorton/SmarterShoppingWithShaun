a2f15e6dd67001e17d19b5b6dc03b7c2218575f4
shaunnorton <shaunnorton87@gmail.com>
2020-10-14 09:36:53 -0700

    Created a function to format tax rate

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function calculate_total(){
     let gross_price = 0
     const tax_input = document.querySelector("#tax-input")
     let net_price = 0
-    let tax_multiplier = (tax_input.value/100)+1
+    let tax_multiplier = (format_value('remove',tax_input,'')/100)+1
     for(let i = 0;i < input_prices.length; i++){
         if(input_prices[i].value != 0){
             gross_price += parseFloat(input_prices[i].value)
@@ -37,5 +37,28 @@ function calculate_total(){
     }
     net_price = parseFloat(gross_price*tax_multiplier,2)
     total.textContent = `Total: $${net_price}`
+    format_value('add',tax_input,"%")
 }
 //----------------------------------------------------------------
+/*Formats the provided inputs value to remove all characters not numbers
+or to add a percentage or dollar sign */
+function format_value(perform,item,symbol){
+    let newvalue =  ''
+    if(perform === 'remove'){
+        for(let i = 0;i<item.value.length;i++){
+            if('1234567890.'.includes(item.value[i])){
+                newvalue+=item.value[i]
+            }
+        }
+        return newvalue
+    }
+    else if(perform === 'add') { 
+        for(let i = 0;i<item.value.length;i++){
+            if(symbol.includes(item.value[i])){
+                item.value = item.value.replace(symbol,'') 
+            }
+        }
+        if(symbol == '%'){item.value = `${item.value}${symbol}`}
+        if(symbol == '$'){item.value = `${symbol}${parseFloat(item.value).toFixed(2)}`}
+    }
+}
