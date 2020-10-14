afd86e64948c0a530c4cd20b55b82fb8341c5d13
shaunnorton <shaunnorton87@gmail.com>
2020-10-14 09:23:47 -0700

    Added comments and unfunctional buttons for saving and loading

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const prices = document.querySelector("#prices")
 let number_of_items = 10
 const tax_rate = document.querySelector("#TaxRate")
 const total = document.querySelector("#Total")
+for(let i = 0; i<number_of_items;i++){
+    addItem()
+}
+tax_rate.addEventListener("input",calculate_total)
+
+//---------------------------------------------------------------
+/*Function that adds list items containing an input to both the shoping list and the price list 
+adds an event listener to the price list item to calculate total. */
 function addItem() {
     let new_item = document.createElement("li")
     new_item.innerHTML= `<input type="text" class="list_item" value="" placeholder="Shopping List Item">`
@@ -12,6 +20,10 @@ function addItem() {
     prices.appendChild(new_price)
     new_price.addEventListener("input",calculate_total)
 }
+//---------------------------------------------------------------
+/*Calculates the total by adding all values from the price list and 
+multiplying by the percentage provided in the tax-rate input. Sets the text of the 
+Total section to the Total: + calculated total */
 function calculate_total(){
     const input_prices = document.querySelectorAll(".price_item")
     let gross_price = 0
@@ -20,16 +32,10 @@ function calculate_total(){
     let tax_multiplier = (tax_input.value/100)+1
     for(let i = 0;i < input_prices.length; i++){
         if(input_prices[i].value != 0){
-            
             gross_price += parseFloat(input_prices[i].value)
-            
         }
     }
     net_price = parseFloat(gross_price*tax_multiplier,2)
-    
     total.textContent = `Total: $${net_price}`
 }
-for(let i = 0; i<number_of_items;i++){
-    addItem()
-}
-tax_rate.addEventListener("input",calculate_total)
+//----------------------------------------------------------------
